8f94a862416f91d4d012ac0d10b90ee81cd61a6c
AnaRenchen <anamagbh@gmail.com>
2025-02-26 16:04:26 -0300

    cambios estilos y add nav

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,6 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  let x = document.getElementById("myTopnav");
-  if (x.classList.contains("responsive")) {
-    x.classList.remove("responsive");
-  } else {
-    x.classList.add("responsive");
-  }
-}
+const mobileMenu = document.getElementById("mobile-menu");
+const navList = document.querySelector(".nav-list");
+
+mobileMenu.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
