7a74117483ba02ef30e57138187c99701c246b63
AnaRenchen <anamagbh@gmail.com>
2025-03-02 19:02:22 -0300

    correcction flecha

--- src/public/js/scrollUp.js
+++ src/public/js/scrollUp.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const scrollToTopButton = document.getElementById("scrollToTop");
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY > 500) {
+    if (window.scrollY > 450) {
       scrollToTopButton.style.display = "flex"; // Mostrar la flecha
     } else {
       scrollToTopButton.style.display = "none"; // Ocultar la flecha
